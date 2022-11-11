29540f635cc6f91d2e6ab34090a0ddb2d1ff7063
Amelia Risner <amelia.risner0@gmail.com>
2022-11-11 15:56:19 -0800

    clicking on grid now toggles between players

--- src/App.js
+++ src/App.js
@@ -4,21 +4,44 @@ import Square from "./Components/Square"
 
 function App() {
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", "", ""])
-  const chooseSuare = () => {
-    
-  }
+  const [player, setPlayer] = useState("X");
+
+  const chooseSquare = (square) => {
+    setBoard(board.map((val, idx) => {
+      if (idx == square && val == "") {
+        console.log(idx)
+        return player
+      }
+
+      return val;
+    }))
+
+    if (player == "X") {
+      setPlayer("O")
+    } else {
+      setPlayer("X");
+    }
+  };
 
 
   return (
     <div className="App">
       <div className="board">
         <div className="row">
-        <Square val={board[0]} chooseSquare={() => {alert(0)}}/>
-        <Square val={board[1]} chooseSquare={() => {alert(1)}}/>
-        <Square val={board[2]} chooseSquare={() => {alert(2)}}/>
+        <Square val={board[0]} chooseSquare={() => {chooseSquare(0)}}/>
+        <Square val={board[1]} chooseSquare={() => {chooseSquare(1)}}/>
+        <Square val={board[2]} chooseSquare={() => {chooseSquare(2)}}/>
         </div>
-        <div className="row"></div>
-        <div className ="row"></div>
+        <div className="row">
+        <Square val={board[3]} chooseSquare={() => {chooseSquare(3)}}/>
+        <Square val={board[4]} chooseSquare={() => {chooseSquare(4)}}/>
+        <Square val={board[5]} chooseSquare={() => {chooseSquare(5)}}/>
+        </div>
+        <div className ="row">
+        <Square val={board[6]} chooseSquare={() => {chooseSquare(6)}}/>
+        <Square val={board[7]} chooseSquare={() => {chooseSquare(7)}}/>
+        <Square val={board[8]} chooseSquare={() => {chooseSquare(8)}}/>
+        </div>
       </div>
     </div>
   );
